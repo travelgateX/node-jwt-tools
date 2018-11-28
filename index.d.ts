3e4a3b70490684c60a09eca8a3ea0054e71152fb
Martí Crespí <mcrespi@xmltravelgate.com>
2018-11-28 10:48:11 +0100

    fixed index location

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export { UserConfig } from './src/classes/user-config';
+export { PermissionTable, Jwt, GroupTree } from './src/classes/permission-table';
+export { JwtTools } from "./src/jwt-tool";
+
--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { UserConfig } from './classes/user-config';
-export { PermissionTable, Jwt, GroupTree } from './classes/permission-table';
-export { JwtTools } from "./jwt-tool";
-
